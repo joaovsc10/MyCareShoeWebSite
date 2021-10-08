aa778413913a2838d081eafe8ffe52861bc7386e
joaovsc10 <joaovitorcoelho10@gmail.com>
2021-10-08 10:03:33 +0100

    changes

--- site/contacts.php
+++ site/contacts.php
@@ -108,6 +108,6 @@
     <div class="snackbars" id="form-output-global"></div>
     <script src="js/core.min.js"></script>
     <script src="js/script.js"></script>
-    <!--coded by Drel-->
+
   </body>
 </html>
