ab2e865490ea57e44c748069e255e1047ee5973d
joaovsc10 <joaovitorcoelho10@gmail.com>
2021-10-16 19:08:10 +0100

    changes icon

--- site/user_permissions.php
+++ site/user_permissions.php
@@ -35,7 +35,7 @@ $profile_id = filter_input(INPUT_POST, 'profile_id', FILTER_SANITIZE_STRING);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="images/mycareshoeicon.co" type="image/x-icon">
   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:300,400,700%7CPoppins:300,400,500,700,900">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/fonts.css">
